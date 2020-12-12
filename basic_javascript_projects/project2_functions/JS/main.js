24d2f084fdbff4ad56adf49954589d08e70066ce
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2020-12-11 23:18:59 -0700

    add comments

--- basic_javascript_projects/project2_functions/JS/main.js
+++ basic_javascript_projects/project2_functions/JS/main.js
@@ -1,13 +1,13 @@
 function cats_dogs() {
-    document.getElementById("my_text").innerHTML="That's better";
-    document.getElementById("my_img").src="./images/dog.jpg";
+    document.getElementById("my_text").innerHTML="That's better"; //Changes text inside the element
+    document.getElementById("my_img").src="./images/dog.jpg"; // changes the src of the image
 }
 
 
 
 function con() {
-    var string_1="I like";
+    var string_1="I like";  // variable assignments
         string_1 += " bananas";
-    document.getElementById("concatenate").innerHTML=string_1;
+    document.getElementById("concatenate").innerHTML=string_1; //assigns the string to the element
 }
 
