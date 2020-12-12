c22805e078524124d93344767a94fe10abdb8a22
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2020-12-11 22:21:43 -0700

    learn variable assignment, popup alerts, expressions, fontcolor method, write method

---
+++ basic_javascript_projects/project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+// popup alert
+// alert('Hello world');
+// window.alert('Hello, world!');
+
+// Assign Variable
+// var a = "First Variable Assignment";
+// multiple variable assignment
+var c='I like to eat,'+' eat,'+' eat,'+' eeples and baneenees!', 
+    mom = "Hermione", 
+    dad = "jeremiah";
+
+// // // Strings:
+// document.write('Hello, World!');
+// // special characters: '\'
+// document.write('Jeff always says "I love to eat food!" but it\'s not necessarily a good thing!');
+// // Concatenate a string
+// document.write('I like to eat,'+' eat,'+' eat,'+' eeples and baneenees!');
+// // fontcolor method
+var yellow=c.fontcolor("yellow");
+var blue=mom.fontcolor("blue");
+var green=dad.fontcolor("green");
+
+
+
+
+
+
+
+// output
+document.write(yellow);
+document.write(blue);
+document.write(green);
