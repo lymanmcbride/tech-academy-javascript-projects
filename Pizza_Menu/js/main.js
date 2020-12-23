fb4be7e0608d9f8eb3ebc6299f382ffe1d1c681d
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2020-12-23 07:33:27 -0700

    add comments to javascript

--- Pizza_Menu/js/main.js
+++ Pizza_Menu/js/main.js
@@ -10,6 +10,7 @@ function getReceipt() {
             text1+= selectedSize+"</p>";
         }
     }
+    // Pizza prices
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     }
@@ -26,7 +27,8 @@ function getReceipt() {
         sizeTotal = 17;
     }
     runningTotal = sizeTotal;
-    console.log(selectedSize+" = $"+sizeTotal+".00");
+    //print subtotal to console log
+    console.log(selectedSize+" = $"+sizeTotal+".00"); 
     console.log("size text1: "+text1);
     console.log("subtotal: $"+runningTotal+".00");
     //these variables will get passed on to each function
@@ -38,6 +40,7 @@ function getTopping(runningTotal,text1) {
     var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
     text1 += "<h3>Added toppings:</h3><p>"
+    // adds the toppings to the console log
     for (var j = 0; j< toppingArray.length; j++) {
         if (toppingArray[j].checked) {
             selectedTopping.push(toppingArray[j].value);
@@ -46,6 +49,7 @@ function getTopping(runningTotal,text1) {
         }
     }
     text1 += "</p>"
+    //gets the total amount of toppings ordered. 1$ per topping, so the total will just be added to with the length of the selected toppings array
     var toppingCount=selectedTopping.length;
     if (toppingCount > 1) {
         toppingTotal = (toppingCount - 1);
@@ -54,10 +58,12 @@ function getTopping(runningTotal,text1) {
         toppingTotal=0;
     }
     runningTotal += toppingTotal;
+    //print the rest of the receipt to the console
     console.log("total selected topping items: "+toppingCount);
     console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
     console.log("topping text1: "+text1);
     console.log("Purchase Total: "+"$"+runningTotal+".00");
+    //print the necessary info to the html page
     document.getElementById("showText").innerHTML=text1;
     document.getElementById("totalPrice").innerHTML="<h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
 };
